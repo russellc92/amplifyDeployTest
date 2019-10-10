2fc6b3604fa5e2309dbc70d26bedf8f8ab95e2be
russellc92 <russell_claxton@hotmail.com>
2019-10-10 20:45:47 +0100

    added honk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import honk from './assets/honk.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={honk} className="App-logo" alt="logo" />
         <h1>Dank</h1>
       </header>
     </div>
